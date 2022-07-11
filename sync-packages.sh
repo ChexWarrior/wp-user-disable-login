8729f62d752cb9f7d8e37d1b60522c95bacebf50
Andrew Lehman <aplehm@gmail.com>
2022-07-11 16:07:57 -0400

    Properly exclude custom plugin

--- sync-packages.sh
+++ sync-packages.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 rsync -az --delete \
-	--exclude web/wp-content/plugins/user-disable packages/plugins/ web/wp-content/plugins
+	--exclude /user-disable/ packages/plugins/ web/wp-content/plugins
