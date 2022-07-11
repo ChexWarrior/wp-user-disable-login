1d9b273f327643ebc15e4ed43637661dadd25a9b
Andrew Lehman <aplehm@gmail.com>
2022-07-11 16:36:00 -0400

    Add scaffold files for plugin

---
+++ web/wp-content/plugins/user-disable/user-disable.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     User Disable
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     user-disable
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         User_Disable
+ */
+
+// Your code starts here.
