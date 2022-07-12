d00b53f16c137a171be87bff838c77642e695067
Andrew Lehman <aplehm@gmail.com>
2022-07-12 09:12:57 -0400

    Add field to user form

--- web/wp-content/plugins/user-disable/user-disable.php
+++ web/wp-content/plugins/user-disable/user-disable.php
@@ -1,4 +1,5 @@
-<?php
+<?php declare(strict_types=1);
+
 /**
  * Plugin Name:     User Disable
  * Plugin URI:      PLUGIN SITE HERE
@@ -12,4 +13,56 @@
  * @package         User_Disable
  */
 
-// Your code starts here.
+namespace Nycedc;
+
+use WP_User;
+
+// Add field, setup CRUD on user page
+
+function usermeta_form_field_disabled(WP_User $user)
+{
+	?>
+	<h3>Disable User Login</h3>
+	<table class="form-table">
+		<tr>
+			<th>
+				<label for="disabled">Is Disabled</label>
+			</th>
+			<td>
+				<input type="checkbox"
+					id="disabled"
+					name="disabled"
+					<?php
+						esc_attr(get_user_meta($user->ID, 'disabled', false))
+							? 'checked' : ''
+					?>
+					title="If checked user will not be able to login"
+					required>
+				<p class="description">
+					If checked user will not be able to login.
+				</p>
+			</td>
+		</tr>
+	</table>
+	<?php
+}
+
+function usermeta_form_field_disabled_update(int $user_id)
+{
+	if (!current_user_can('edit_user', $user_id)) {
+		return false;
+	}
+
+	$disabled = $_POST['disabled'] === 'true';
+
+	return update_user_meta(
+		$user_id,
+		'disabled',
+		$disabled
+	);
+}
+
+add_action('show_user_profile', 'usermeta_form_field_disabled');
+add_action('edit_user_profile', 'usermeta_form_field_disabled');
+add_action('personal_options_update', 'usermeta_form_field_disabled_update');
+add_action('edit_user_profile_update', 'usermeta_form_field_disabled_update');
