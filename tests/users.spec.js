dc1c1b3eb654ecd89b7430ccd15852aa0a81bba2
Andrew Lehman <alehman@edc.nyc>
2022-07-13 16:05:33 -0400

    Add admins cannot be disabled test

--- tests/users.spec.js
+++ tests/users.spec.js
@@ -3,16 +3,9 @@ const { test, expect } = require('./fixture.js');
 
 const adminUsername = 'admin';
 const authorUserId = 2;
+const adminUserId = 3;
 
-// Test that a disabled user cannot login
-
-// Test that a disabled user loses their current session
-
-// Test that a disabled user who is enabled can login
-
-// Test that admins cannot be disabled
-
-test('Admins can disable and enable another user', async ({ userProfile, page }) => {
+test('Admins can disable and enable another user', async ({ userProfile }) => {
   await userProfile.login(adminUsername);
   await userProfile.disableUser(authorUserId);
   let checked = await userProfile.disabledCheckbox.isChecked();
@@ -21,5 +14,21 @@ test('Admins can disable and enable another user', async ({ userProfile, page })
   checked = await userProfile.disabledCheckbox.isChecked();
   await expect(checked).toBeFalsy();
 });
+
+test('Admins cannot be disabled', async({ userProfile }) => {
+  await userProfile.login(adminUsername);
+  await userProfile.gotoUserProfile(adminUserId);
+  const exists = await userProfile.disabledCheckbox.count() > 0;
+
+  await expect(exists).toBeFalsy();
+});
+
+// Test that a disabled user cannot login
+
+// Test that a disabled user loses their current session
+
+// Test that a disabled user who is enabled can login
+
+
 
 
