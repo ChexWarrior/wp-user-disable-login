1b832d877b064da0dad59d53a764a6c08b3b3d54
Andrew Lehman <alehman@edc.nyc>
2022-07-13 13:54:53 -0400

    Remove assertions from login/logout

--- tests/models/userAuth.js
+++ tests/models/userAuth.js
@@ -1,5 +1,3 @@
-const { expect } = require('@playwright/test');
-
 /**
  * Class for logging user into and out of WordPress site
  */
@@ -16,8 +14,6 @@ class UserAuth {
     await this.page.locator('#user_login').fill(username);
     await this.page.locator('#user_pass').fill(password);
     await this.page.locator('#wp-submit').click();
-    const accountMenu = this.page.locator('#wp-admin-bar-my-account');
-    await expect(accountMenu).toBeVisible();
   }
 
   async logout() {
@@ -25,7 +21,6 @@ class UserAuth {
     const logoutUrl = await logoutLink.getAttribute('href');
     await this.page.goto(logoutUrl);
     const url = this.page.url();
-    await expect(/\?loggedout\=true/.test(url)).toBeTruthy();
   }
 }
 
