ac3064aef59d99387b0dd4925246a1fe185e8faf
Andrew Lehman <alehman@edc.nyc>
2022-07-13 14:31:09 -0400

    Add test for disabling a user as admin

--- tests/users.spec.js
+++ tests/users.spec.js
@@ -3,14 +3,25 @@ const { test, expect } = require('./fixture.js');
 
 // Test that a disabled user cannot login
 
-
 // Test that a disabled user loses their current session
 
 // Test that a disabled user who is enabled can login
 
 // Test that admins cannot be disabled
 
-test('basic test', async({ userAuth, page }) => {
-  await userAuth.login('admin');
-  await userAuth.logout();
+// Test that only admins can disable a user
+test('Only Admins can disable another user', async ({ userProfile, page }) => {
+  await userProfile.login('admin');
+
+  // Go to author user profile page
+  await userProfile.gotoUserProfile(2);
+  await userProfile.disabledCheckbox.check();
+  await userProfile.updateButton.click();
+  const checked = await userProfile.disabledCheckbox.isChecked();
+  const message = await userProfile.page.locator('#message').innerText();
+
+  await expect(message.includes('User updated')).toBeTruthy();
+  await expect(checked).toBeTruthy();
 });
+
+// Test that only admins can enable a user
