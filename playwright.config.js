e0c71e962fe9e4c7440434b431e7caf52e234a6f
Andrew Lehman <alehman@edc.nyc>
2022-07-13 12:03:51 -0400

    Add automatic screenshots on failure

--- playwright.config.js
+++ playwright.config.js
@@ -2,6 +2,8 @@ const config = {
   use: {
     viewport: { width: 1280, height: 720 },
     ignoreHTTPSErrors: true,
+    screenshot: 'only-on-failure',
+    baseURL: 'https://user-disable-test.ddev.site:8443',
   }
 };
 
