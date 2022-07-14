79debe2bc642abdc84bcdcdec44b81fa553e694a
Andrew Lehman <alehman@edc.nyc>
2022-07-14 09:52:22 -0400

    Make test user names follow pattern

--- tests/users.spec.js
+++ tests/users.spec.js
@@ -2,58 +2,58 @@ const { UserProfile } = require('./models/userProfile.js');
 const { test, expect } = require('./fixture.js');
 
 // Test user information
-const adminInfo = { username: 'admin', id: 1 };
-const authorInfo = { username: 'author1', id: 2 };
+const admin1Info = { username: 'admin1', id: 1 };
+const author1Info = { username: 'author1', id: 2 };
 const admin2Info = { username: 'admin2', id: 3 };
 const author2Info = { username: 'author2', id: 4 };
 
 test('Admins can disable and enable another user', async ({ userProfile }) => {
-  await userProfile.login(adminInfo.username);
-  await userProfile.disableUser(authorInfo.id);
+  await userProfile.login(admin1Info.username);
+  await userProfile.disableUser(author1Info.id);
   let checked = await userProfile.disabledCheckbox.isChecked();
   await expect(checked).toBeTruthy();
-  await userProfile.enableUser(authorInfo.id);
+  await userProfile.enableUser(author1Info.id);
   checked = await userProfile.disabledCheckbox.isChecked();
   await expect(checked).toBeFalsy();
 });
 
 test('Admins cannot be disabled', async({ userProfile }) => {
-  await userProfile.login(adminInfo.username);
-  await userProfile.gotoUserProfile(adminInfo.id);
+  await userProfile.login(admin1Info.username);
+  await userProfile.gotoUserProfile(admin1Info.id);
   const exists = await userProfile.disabledCheckbox.count() > 0;
 
   await expect(exists).toBeFalsy();
 });
 
 test.only('A disabled user cannot login', async({ userProfile }) => {
-  await userProfile.login(adminInfo.username);
-  await userProfile.disableUser(authorInfo.id);
+  await userProfile.login(admin1Info.username);
+  await userProfile.disableUser(author1Info.id);
   await userProfile.logout();
-  await userProfile.login(authorInfo.username);
+  await userProfile.login(author1Info.username);
   const messageDiv = userProfile.page.locator('#login_error');
   const errorMsg = await messageDiv.innerText();
 
   await expect(errorMsg.includes('User is disabled')).toBeTruthy();
 
   // Clean up
-  await userProfile.login(adminInfo.username);
-  await userProfile.enableUser(authorInfo.id);
+  await userProfile.login(admin1Info.username);
+  await userProfile.enableUser(author1Info.id);
 });
 
 test('Disabled users lose active login sessions', async ({ userProfile, otherUserProfile }) => {
   // Login as user who will be disabled
-  await userProfile.login(authorInfo.username);
+  await userProfile.login(author1Info.username);
 
   // Login as admin via a new session and disable user
   await otherUserProfile.login(admin2Info.username);
-  await otherUserProfile.disableUser(authorInfo.id);
+  await otherUserProfile.disableUser(author1Info.id);
 
   // Verify that disabled user is now logged out
-  await userProfile.gotoUserProfile(authorInfo.id);
+  await userProfile.gotoUserProfile(author1Info.id);
   const authorUrl = userProfile.page.url();
 
   expect(authorUrl.includes('wp-login.php')).toBeTruthy();
 
   // Clean up
-  await otherUserProfile.enableUser(authorInfo.id);
+  await otherUserProfile.enableUser(author1Info.id);
 });
