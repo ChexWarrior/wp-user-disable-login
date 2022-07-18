671f76ee0b3c799a29926b2d7752db6b275aec1a
Andrew Lehman <alehman@edc.nyc>
2022-07-18 15:02:41 -0400

    Properly exclude custom plugin from deletion in rsync

--- scripts/sync-packages.sh
+++ scripts/sync-packages.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 rsync -az --delete \
-	--exclude /user-disable/ packages/plugins/ web/wp-content/plugins
+	--exclude /user-login-disable/ packages/plugins/ web/wp-content/plugins
