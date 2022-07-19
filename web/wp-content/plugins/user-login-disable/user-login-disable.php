a58f569439af27f32ca8b3b726239f57c16f2e22
Andrew Lehman <aplehm@gmail.com>
2022-07-19 15:56:46 -0400

    Update registered command names

--- web/wp-content/plugins/user-login-disable/user-login-disable.php
+++ web/wp-content/plugins/user-login-disable/user-login-disable.php
@@ -21,6 +21,6 @@ if (defined('WP_CLI') && !empty(WP_CLI)) {
 	// Register our commands
 	$cli_commmands = new UserLoginDisableCmds(UserLoginDisablePlugin::getInstance());
 
-	WP_CLI::add_command('user enable', [$cli_commmands, 'enable_users']);
-	WP_CLI::add_command('user disable', [$cli_commmands, 'disable_users']);
+	WP_CLI::add_command('user enable', [$cli_commmands, 'enableUsers']);
+	WP_CLI::add_command('user disable', [$cli_commmands, 'disableUsers']);
 }
