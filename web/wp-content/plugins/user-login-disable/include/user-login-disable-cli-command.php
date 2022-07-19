75a34d9abb2b4a1e523636a3b96b25f27687fd57
Andrew Lehman <aplehm@gmail.com>
2022-07-19 11:40:52 -0400

    Wp cli command accepts user login, email or ID

--- web/wp-content/plugins/user-login-disable/include/user-login-disable-cli-command.php
+++ web/wp-content/plugins/user-login-disable/include/user-login-disable-cli-command.php
@@ -27,43 +27,35 @@ class User_Login_Disable_CLI_Command
 	 *
 	 * ## OPTIONS
 	 *
-	 * [user_ids]
+	 * [<user_ids>...]
 	 * : A list of user ids for the users to be enabled
 	 *
 	 * [--all]
 	 * : If this flag is included then all users in site will be enabled
 	 *
 	 */
-	public function enable_users(array $user_ids = [], array $assoc_args = []): void
+	public function enable_users(array $user_args = [], array $assoc_args = []): void
 	{
-        /**
-         * Check if the --all flag was passed, if so we grab all user ids and
-         * enable them
-         *
-         * If no all flag then determine grab passed user identifiers and delete each (can be username, useremail or id)
-         */
 		['all' => $allFlag] = $assoc_args;
-
-		if ($allFlag) {
-			// Get all user ids
-            $user_ids = $this->run_user_query(true, false);
-		} else {
-			$this->verify_user_ids($user_ids);
-		}
+		$user_ids = $this->run_user_query(
+			$allFlag === true,
+			false,
+			$user_args,
+		);
 
 		$count = $this->userLoginDisable->enable_disable_users('enable_user', $user_ids);
 
 		WP_CLI::success("Enabled $count user(s)");
 	}
 
-    /**
-     * Builds user query for disabling or enabling users via WP_CLI
-     *
-     * @param bool $getAll
-     * @param bool $enabled - If true we are getting users who are enabled, otherwise we're getting users who are disabled
-     * @param array $userArgs
-     */
-    private function run_user_query(bool $getAll, bool $enabled, array $userArgs = []): array
+	/**
+	 * Builds user query for disabling or enabling users via WP_CLI
+	 *
+	 * @param bool $getAll
+	 * @param bool $getEnabledUsers - If true we are getting users who are enabled, otherwise we're getting users who are disabled
+	 * @param array $userArgs
+	 */
+    private function run_user_query(bool $getAll, bool $getEnabledUsers, array $userArgs = []): array
     {
         // Default to getting enabled users
         $meta = [
@@ -80,21 +72,78 @@ class User_Login_Disable_CLI_Command
             ],
         ];
 
-        if (!$enabled) {
+        if (!$getEnabledUsers) {
             $meta = [
                 'meta_key' => 'disabled',
                 'meta_value' => 'disabled',
             ];
         }
 
+		if ($getAll) {
+			$query_args = [
+				'fields' => 'ID',
+				'role__not_in' => 'administrator',
+				'meta_query' => $meta,
+			];
+
+			return get_users($query_args);
+		}
+
+		['user_ids' => $ids, 'user_name_emails' => $names_emails] = $this->split_ids_from_names_emails($userArgs);
+
         $query_args = [
-            'fields' => 'ID',
+            'fields' => ['ID', 'user_login', 'user_email'],
             'role__not_in' => 'administrator',
             'meta_query' => $meta,
         ];
 
-        return get_users($query_args);
+        $user_info = get_users($query_args);
+
+		// Filter out users who don't match an ID, name or email param
+		$user_info = array_filter($user_info, function($i) use ($ids, $names_emails) {
+			if (in_array($i->ID, $ids)) {
+				return true;
+			}
+
+			if (in_array($i->user_login, $names_emails)) {
+				return true;
+			}
+
+			if (in_array($i->user_email, $names_emails)) {
+				return true;
+			}
+
+			return false;
+		});
+
+		return array_map(fn($i) => $i->ID, $user_info);
     }
+
+	/**
+	 * Takes a list of user emails, user names and ids and returns
+	 * an array with two values: one contains an array of the user ids passed in
+	 * and another array with all the usernames and emails
+	 * @param array $args
+	 * @return array
+	 */
+	private function split_ids_from_names_emails(array $args): array
+	{
+		$non_ids = [];
+		$ids = array_filter($args, function($i) use (&$non_ids) {
+			if (is_numeric($i)) {
+				return true;
+			}
+
+			$non_ids[] = $i;
+
+			return false;
+		});
+
+		return [
+			'user_ids' => $ids,
+			'user_name_emails' => $non_ids,
+		];
+	}
 
 	/**
 	 * Disable users
