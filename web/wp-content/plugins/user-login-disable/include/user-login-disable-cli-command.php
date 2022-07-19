4f2edd87e39da78cd1c6c09e5a2cc4d05918c70d
Andrew Lehman <aplehm@gmail.com>
2022-07-19 10:35:58 -0400

    Add --all flag for enabling users

--- web/wp-content/plugins/user-login-disable/include/user-login-disable-cli-command.php
+++ web/wp-content/plugins/user-login-disable/include/user-login-disable-cli-command.php
@@ -27,19 +27,26 @@ class User_Login_Disable_CLI_Command
 	 *
 	 * ## OPTIONS
 	 *
-	 * <user_ids>
+	 * [user_ids]
 	 * : A list of user ids for the users to be enabled
 	 *
 	 * [--all]
 	 * : If this flag is included then all users in site will be enabled
 	 *
 	 */
-	public function enable_users(array $user_ids, array $assoc_args): void
+	public function enable_users(array $user_ids = [], array $assoc_args = []): void
 	{
+        /**
+         * Check if the --all flag was passed, if so we grab all user ids and
+         * enable them
+         *
+         * If no all flag then determine grab passed user identifiers and delete each (can be username, useremail or id)
+         */
 		['all' => $allFlag] = $assoc_args;
 
 		if ($allFlag) {
 			// Get all user ids
+            $user_ids = $this->run_user_query(true, false);
 		} else {
 			$this->verify_user_ids($user_ids);
 		}
@@ -48,6 +55,46 @@ class User_Login_Disable_CLI_Command
 
 		WP_CLI::success("Enabled $count user(s)");
 	}
+
+    /**
+     * Builds user query for disabling or enabling users via WP_CLI
+     *
+     * @param bool $getAll
+     * @param bool $enabled - If true we are getting users who are enabled, otherwise we're getting users who are disabled
+     * @param array $userArgs
+     */
+    private function run_user_query(bool $getAll, bool $enabled, array $userArgs = []): array
+    {
+        // Default to getting enabled users
+        $meta = [
+            'meta_query' => [
+                'relation' => 'OR',
+                [
+                    'key' => 'disabled',
+                    'value' => '',
+                ],
+                [
+                    'key' => 'disabled',
+                    'compare' => 'NOT EXISTS',
+                ]
+            ],
+        ];
+
+        if (!$enabled) {
+            $meta = [
+                'meta_key' => 'disabled',
+                'meta_value' => 'disabled',
+            ];
+        }
+
+        $query_args = [
+            'fields' => 'ID',
+            'role__not_in' => 'administrator',
+            'meta_query' => $meta,
+        ];
+
+        return get_users($query_args);
+    }
 
 	/**
 	 * Disable users
