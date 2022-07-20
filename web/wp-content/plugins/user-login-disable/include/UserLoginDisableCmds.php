94ca8cab474a7ad8630a949a9e4f5834b63618c7
Andrew Lehman <alehman@edc.nyc>
2022-07-20 09:45:41 -0400

    Setup autoload within plugin

--- web/wp-content/plugins/user-login-disable/include/UserLoginDisableCmds.php
+++ web/wp-content/plugins/user-login-disable/include/UserLoginDisableCmds.php
@@ -1,5 +1,7 @@
 <?php declare(strict_types=1);
 
+namespace Chexwarrior;
+
 class UserLoginDisableCmds
 {
 	private UserLoginDisablePlugin $userLoginDisable;
--- web/wp-content/plugins/user-login-disable/user-login-disable.php
+++ web/wp-content/plugins/user-login-disable/user-login-disable.php
@@ -9,20 +9,18 @@
  * @package         User_Login_Disable
  */
 
-require dirname(__FILE__) . '/include/UserLoginDisablePlugin.php';
+require dirname(__FILE__) . '/vendor/autoload.php';
 
  // Instantiate the class
-$user_login_disable = UserLoginDisablePlugin::getInstance();
+$user_login_disable = Chexwarrior\UserLoginDisablePlugin::getInstance();
 
 // Register activation and uninstall hooks
 register_activation_hook(__FILE__, 'UserLoginDisablePlugin::activatePlugin');
 register_uninstall_hook(__FILE__, 'UserLoginDisablePlugin::uninstallPlugin');
 
+// Register our commands
 if (defined('WP_CLI') && !empty(WP_CLI)) {
-	require dirname(__FILE__) . '/include/UserLoginDisableCmds.php';
-
-	// Register our commands
-	$cli_commmands = new UserLoginDisableCmds(UserLoginDisablePlugin::getInstance());
+	$cli_commmands = new Chexwarrior\UserLoginDisableCmds(Chexwarrior\UserLoginDisablePlugin::getInstance());
 
 	WP_CLI::add_command('user enable', [$cli_commmands, 'enableUsers']);
 	WP_CLI::add_command('user disable', [$cli_commmands, 'disableUsers']);
