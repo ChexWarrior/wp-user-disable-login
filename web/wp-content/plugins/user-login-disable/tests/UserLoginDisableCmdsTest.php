aaadf996e0309735d992931483827d30878613b2
Andrew Lehman <alehman@edc.nyc>
2022-07-20 14:03:12 -0400

    Trim shell command result

--- web/wp-content/plugins/user-login-disable/tests/UserLoginDisableCmdsTest.php
+++ web/wp-content/plugins/user-login-disable/tests/UserLoginDisableCmdsTest.php
@@ -4,18 +4,53 @@ use PHPUnit\Framework\TestCase;
 use Chexwarrior\UserLoginDisablePlugin;
 use Chexwarrior\UserLoginDisableCmds;
 
+/**
+ * This is rather hacky but in order to test our wp-cli commands properly we'll
+ * run them via shell commands in phpunit against the DDEV site and check
+ * that they have the expected output (wp user meta get)
+ */
 class UserLoginDisableCmdsTest extends TestCase
 {
     private ?UserLoginDisableCmds $cliCmd = null;
+	private string $wpFullPath = '/var/www/html/web';
 
-    protected function setUp(): void
-    {
-        $stubPlugin = $this->createStub(UserLoginDisablePlugin::class);
-        $this->cliCmd = new UserLoginDisableCmds($stubPlugin);
-    }
+	protected function runWpCliCmd(string $cmd, array $params)
+	{
+		$paramStr = implode(' ', $params);
+		return trim(shell_exec("wp --path={$this->wpFullPath} $cmd $paramStr"));
+	}
 
-    public function testFilterUsersByArgs()
+	protected function checkUserDisabled(string $userId)
+	{
+		return $this->runWpCliCmd('user meta get', [$userId, 'disabled']) === '1';
+	}
+
+	protected function checkUserEnabled(string $userId)
+	{
+		return $this->runWpCliCmd('user meta get', [$userId, 'disabled']) !== '1';
+	}
+
+	protected function disableEnableUser(bool $disable, string $userId, bool $all = false)
+	{
+		$params = [$userId];
+
+		if ($all) $params[] = '--all';
+
+		$action = $disable ? 'disable' : 'enable';
+
+		return $this->runWpCliCmd("user $action", $params);
+	}
+
+    // protected function setUp(): void
+    // {
+    //     $stubPlugin = $this->createStub(UserLoginDisablePlugin::class);
+    //     $this->cliCmd = new UserLoginDisableCmds($stubPlugin);
+    // }
+
+    public function testUserCanBeDisabled()
     {
-        $this->assertSame(1, 1);
+        $this->disableEnableUser(true, '2');
+
+		$this->assertTrue($this->checkUserDisabled('2'));
     }
 }
